457a2fce5b14bebadf8e41753f4a580f0e135462
Shubham Bandagale <sbandagale18@gmail.com>
2021-09-06 13:56:31 +0530

    ADDED: To check if the email follows specifications

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String FIRST_NAME_PATTERN= ("^[A-Z]{1}[a-z]{2,}");
     private static final String LAST_NAME_PATTERN= ("^[A-Z]{1}[a-z]{2,}");
+    private static final String EMAIL_PATTERN= ("[a-zA-Z]+[.]*[a-zA-Z]*[@][b][l][.][c][o][.]*[in]*");
 
     public boolean validFirstName(String firstName){
         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
@@ -16,6 +17,8 @@ public class UserRegistration {
         Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
         return pattern.matcher(lastName).matches();
     }
-
-
-}
+    public boolean validEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
+    }
+    }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -2,22 +2,23 @@ package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserRegistrationTest {
-
     @Test
     void whenFirstname_startsWithCapital() {
         UserRegistration userRegistration = new UserRegistration();
          boolean result= userRegistration.validFirstName("Sub");
         Assertions.assertTrue(result);
     }
-
     @Test
     void whenLastname_startsWithCapital() {
         UserRegistration userRegistration= new UserRegistration();
         boolean result= userRegistration.validLastName("Ban");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    void whenEmailentered_accordingToSpecifications() {
+        UserRegistration userRegistration= new UserRegistration();
+        boolean result= userRegistration.validEmail("abc.xyz@bl.co.in");
         Assertions.assertTrue(result);
     }
 }
